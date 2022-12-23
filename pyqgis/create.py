c9b463e7e5b07653c00a8116052c6b97d1456851
Uadson Feitosa <uadsonpy@gmail.com>
2022-12-23 19:21:51 -0300

    created module_01

--- pyqgis/create.py
+++
@@ -1,4 +0,0 @@
-from qgis.core import QgsProject()
-
-project = QgsProject.instance()
-print(project)
--- pyqgis/read.py
+++
@@ -1,6 +0,0 @@
-from .create import project
-
-project.read(r'/mnt/DADOS/repos/qgis/qgis_learning/pyqgis/projects')
-print(project.fileName())
-
-print(project)
