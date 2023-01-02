27e4612cbb0d9537eb1445266852e77730769424
Uadson Feitosa <uadsonpy@gmail.com>
2023-01-02 19:35:59 -0300

    class methods for vector part 2

--- pyqgis/auto_shp_open.py
+++ pyqgis/auto_shp_open.py
@@ -1,54 +1,17 @@
 import os
-import qgis.core as qg
-
-from geoscript.models import GeoScript
-
-# 1. directory name files
-data = 'data'
-projects = 'projects'
-
-# 2. creating a object from GeoScript
-project = GeoScript()
-
-# 3. getting files list
-"""project.list_files(data)
-project.list_files(projects)"""
-
-# 4. loading project
-project.instance()
-project.load(projects, 'aeroportos')
-
-"""import os
-
-from pathlib import PureWindowsPath
-from projects import BASE_DIR, projects
-
-import qgis.core as qg
-
-# data path
-data = PureWindowsPath(BASE_DIR).joinpath('../data')
 
 # projects path
-project_path = PureWindowsPath(BASE_DIR).joinpath(str(projects))
+path = '/mnt/DADOS/repos/qgis/qgis_learning/pyqgis/data/'
 
-# new project path
-new_project = PureWindowsPath(project_path).joinpath('aeroportos.qgz')
+project = QgsProject.instance()
 
-# save new project
-project = qg.QgsProject.instance()
-project.write(str(new_project))
-
-# loading this project
-project.read(str(new_project))
-
-for root, directory, files in os.walk(data):
+for root, directory, files in os.walk(path):
     for file in files:
         if file.endswith('.shp'):
-            layer = qg.QgsVectorLayer(
+            layer = QgsVectorLayer(
                 # path + file
-                os.path.join(data, file),
+                os.path.join(path, file),
                 # file.shp -> file
                 file[0:-4],
                 "ogr")
             project.addMapLayer(layer)
-"""
--- pyqgis/methods_for_vectors_pt_2.py
+++ pyqgis/methods_for_vectors_pt_2.py
@@ -1,16 +1,39 @@
-import os
-import ogr
-import gdal
-import qgis.core
+path = '/mnt/DADOS/repos/qgis/qgis_learning/pyqgis/data/'
+filename = 'aeroportos.shp'
 
-path = ''
-
-
-"""filename = 'aeroportos.shp'
-    
+# QgsVectorLayer(file_path:str, filename:str, ogr:str)    
 layer = QgsVectorLayer(os.path.join(path, filename), filename[0:-4].title(), "ogr")
 
+# Add Layer
 QgsProject.instance().addMapLayer(layer)  
 
-# Getting features
-layer.getFeatures()"""
+# Add attributes
+layer.startEditing()
+layer.addAttribute(QgsField('x', QVariant.Double))
+layer.commitChanges()
+
+# Common types: String, Double, Int
+
+count = 0
+for feature in layer.getFeatures():
+    while count < 5:
+        print(feature.geometry())
+        count += 1
+        
+count = 0
+for feature in layer.getFeatures():
+    while count < 5:
+        print(feature.id(), feature.geometry().asPoint()[0])
+        count += 1
+
+layer.fields().count() # 15 -> 0 until 14
+
+layer.startEditing()
+layer.addAttribute(QgsField('x', QVariant.Double))
+for feature in layer.getFeatures():
+    id = feature.id()
+    x = feature.geometry().asPoint()[0]
+    attr_value = {14: x}
+    layer.changeAttributeValues(id, attr_value)
+    
+layer.commitChanges()
