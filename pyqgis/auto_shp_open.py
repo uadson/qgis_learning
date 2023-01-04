b50b27efcfe2c8369ad6ae237b83f93e3383f7ad
Uadson Feitosa <uadsonpy@gmail.com>
2023-01-03 21:20:25 -0300

    refactor code

--- pyqgis/auto_shp_open.py
+++ pyqgis/auto_shp_open.py
@@ -1,17 +1,35 @@
 import os
+import qgis.core as qg
 
 # projects path
 path = '/mnt/DADOS/repos/qgis/qgis_learning/pyqgis/data/'
+path_windows = 'D:\\repos\\qgis\\qgis_learning\\pyqgis\\data\\'
 
-project = QgsProject.instance()
+project = qg.QgsProject.instance()
+def open_layer_lx():
+    for root, directory, files in os.walk(path):
+        for file in files:
+            if file.endswith('.shp'):
+                layer = qg.QgsVectorLayer(
+                    # path + file
+                    os.path.join(path, file),
+                    # file.shp -> file
+                    file[0:-4],
+                    "ogr")
+                project.addMapLayer(layer)
 
-for root, directory, files in os.walk(path):
-    for file in files:
-        if file.endswith('.shp'):
-            layer = QgsVectorLayer(
-                # path + file
-                os.path.join(path, file),
-                # file.shp -> file
-                file[0:-4],
-                "ogr")
-            project.addMapLayer(layer)
+
+def open_layer_wd():
+    for root, directory, files in os.walk(path_windows):
+        for file in files:
+            if file.endswith('.shp'):
+                layer = qg.QgsVectorLayer(
+                    # path + file
+                    os.path.join(path_windows, file),
+                    # file.shp -> file
+                    file[0:-4],
+                    "ogr")
+                project.addMapLayer(layer)
+
+
+open_layer_wd()
