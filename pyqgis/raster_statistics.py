7f59084a977a92ebe3939f09b4d6f5a13377f537
Uadson Feitosa <uadsonpy@gmail.com>
2023-01-12 21:06:38 -0300

    adds raster scripts

---
+++ pyqgis/raster_statistics.py
@@ -0,0 +1,37 @@
+import os
+
+from qgis.core import *
+from PyQt5.QtGui import QColor
+
+
+filepath ='/mnt/DADOS/github/qgis_learning/pyqgis/data/merge.tif'
+
+rlayer = QgsRasterLayer(filepath, "demo")
+
+fcn = QgsColorRampShader()
+fcn.setColorRampType(QgsColorRampShader.Interpolated)
+
+colors = [
+	QgsColorRampShader.ColorRampItem(0, QColor(255, 0, 0)),
+	QgsColorRampShader.ColorRampItem(300, QColor(255, 153, 0)),
+	QgsColorRampShader.ColorRampItem(900, QColor(255, 255, 102)),
+	QgsColorRampShader.ColorRampItem(1100, QColor(153, 255, 102)),
+	QgsColorRampShader.ColorRampItem(1600, QColor(0, 51, 0)),
+]
+
+fcn.setColorRampItemList(colors)
+shader = QgsRasterShader()
+
+shader.setRasterShaderFunction(fcn)
+
+renderer = QgsSingleBandPseudoColorRenderer(rlayer.dataProvider(), 1, shader)
+
+rlayer.setRenderer(renderer)
+
+rlayer.triggerRepaint()
+
+project = QgsProject.instance().addMapLayer(rlayer)
+
+statistics = rlayer.dataProvider().bandStatistics(1, QgsRasterBandStats.All)
+
+print(dir(statistics))
